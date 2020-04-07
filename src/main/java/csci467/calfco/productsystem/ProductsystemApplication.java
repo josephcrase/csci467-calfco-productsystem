5e93a1bc8e9433c7cf1a3cf056714a06cb2ed248
joecrase <joecrase@gmail.com>
2020-04-07 11:33:30 -0500

    initial commit

---
+++ src/main/java/csci467/calfco/productsystem/ProductsystemApplication.java
@@ -0,0 +1,13 @@
+package csci467.calfco.productsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/csci467/calfco/productsystem/ProductsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package csci467.calfco.productsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
